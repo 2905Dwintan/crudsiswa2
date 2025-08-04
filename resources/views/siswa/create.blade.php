6c7eb1285d0252070a7200da44d36ea4739909eb
2905Dwintan <dwi.intan2905@gmail.com>
2025-08-04 09:50:52 +0700

    fix(siswa):perbaikan pada create

--- resources/views/siswa/create.blade.php
+++ resources/views/siswa/create.blade.php
@@ -6,7 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Halaman Tambah</h1><br>
+    <h1>Halaman Siswa PPLG </h1><br>
     
 <form action="/siswa/store" method="post">
          @csrf
@@ -19,30 +19,66 @@
             <option value="3">XII PPLG 3</option>
         </select>
     </div><br>
+
+    @error('kelas_id')
+    <small style="color: #FF0000">{{ $message }}</small>
+    @enderror
+
     <div>
+     
         <label for="name">Nama:</label><br>
         <input type="text" name="name">
+        <br>
+        @error('name')
+    <small style="color: #FF0000">{{ $message }}</small>
+    @enderror
     </div><br>
+
     <div>
         <label for="nisn">NISN:</label><br>
         <input type="text" name="nisn">
+        <br>
+        @error('nisn')
+    <small style="color: #FF0000">{{ $message }}</small>
+    @enderror
     </div><br>
+
     <div>
         <label for="alamat">Alamat:</label><br>
         <input type="text" name="alamat">
+        <br>
+        @error('alamat')
+    <small style="color: #FF0000">{{ $message }}</small>
+    @enderror
     </div><br>
+
     <div>
         <label for="no_handphone">No Handphone:</label><br>
         <input type="text" name="no_handphone">
+        <br>
+        @error('no_handphone')
+    <small style="color: #FF0000">{{ $message }}</small>
+    @enderror
     </div><br>
+
     <div>
         <label for="email">Email:</label><br>
         <input type="text" name="email">
+        <br>
+        @error('email')
+    <small style="color: #FF0000">{{ $message }}</small>
+    @enderror
     </div><br>
+
     <div>
         <label for="password">Password:</label><br>
         <input type="password" name="password">
+        <br>
+        @error('password')
+    <small style="color: #FF0000">{{ $message }}</small>
+    @enderror
     </div><br>
+
     <div>
         <label for="photo">Photo</label><br>
         <input type="file" name="photo">
